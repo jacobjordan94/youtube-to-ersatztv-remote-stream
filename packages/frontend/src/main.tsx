7bde7e6d5d46d69249f0cb34a430079fa80e3dd8
Jacob J <jacobjordan94@live.com>
2026-02-17 20:28:21 -0600

    idk man, I always forget to do this

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
+const root = (
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  import.meta.env.PROD ? root : <StrictMode>{ root }</StrictMode>
 );
